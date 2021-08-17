465ba18bd87bfed25e9258566310c8f2db743088
Kaushik <kaushik@Kaushiks-MacBook-Air.local>
2021-08-17 21:39:57 +0530

    fully working front-end

--- src/Components/AddName.js
+++ src/Components/AddName.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import { useForm } from '../Hooks/useForm';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+const AddName = () => {
 
-const AddName = () =>{
+    const [values, HandleChange] = useForm({ childName: "" });
+    const addChildName = (e) => {
+        e.preventDefault();
+        console.log(values);
+        axios.post(process.env.REACT_APP_API_URL + '/addName', values)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(
+                err => {
+                    console.log(err);
+                }
+            )
+    }
 
-    return(
-        <div className="container">
-            <h1>Add Name page</h1>
+    return (
+        <div >
+            <div className="container" >
+                <div className="jumbotron jumbotron-fluid" style={{ textAlign: "center" }}>
+                    <div className="container" style={{ paddingTop: "5%", paddingBottom: "5%" }}>
+                        <h1 className="display-4" style={{ fontWeight: "bold" }}><u>Add Child Name</u></h1>
+                    </div>
+                </div>
+                <form>
+                    <div className="form-group">
+                        <label style={{fontSize:"20px"}}>Child name:</label>
+                        <br />
+                        <input type="text" name="childName" className="form-control" placeholder="Enter Name"
+                            value={values.childName} onChange={HandleChange}
+                        />
+                    </div>
+                    <br />
+                    <button type="submit" className="btn btn-primary" onClick={e => { addChildName(e) }} style={{ width: "100%" }}>Submit</button>
+                    <br /><br />
+                    <Link className="btn btn-primary" to="/" style={{ width: "100%" }}>Back to points table</Link>
+                </form>
+            </div>
         </div>
     );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,105 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-const Home = () =>{
-    return(
+const Home = () => {
+
+    const [childData, setChildData] = useState([]);
+    const [load, setLoad] = useState(false);
+    useEffect(() => {
+        axios.get(process.env.REACT_APP_API_URL + '/getDetails')
+            .then(res => {
+                setChildData(res.data.data);
+                setLoad(false);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [load]);
+
+    const incrementRating = (id) => {
+        axios.patch(process.env.REACT_APP_API_URL + '/increment/' + id)
+            .then(() => {
+                setLoad(true);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    const decrementRating = (id) => {
+        axios.patch(process.env.REACT_APP_API_URL + '/decrement/' + id)
+            .then(() => {
+                setLoad(true);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    return (
         <div className="container">
-            <h1>Home Page</h1>
+            <div className="jumbotron jumbotron-fluid"
+                style={{ textAlign: "center", paddingBottom: "5%", paddingTop: "5%" }}>
+                <div className="container">
+                    <h1 className="display-4"
+                        style={{ fontWeight: "bold" }}>
+                        <u>Points Table</u>
+                    </h1>
+                </div>
+            </div>
+            <div className="row"
+                style={{ textAlign: "center", borderBottom: "1px solid black" }}>
+                <div className="col">
+                    <h4 style={{ fontWeight: "bold" }}>Name</h4>
+                    <Link to="/addName"  style={{ fontSize:"14px", textDecoration: "none" }}>
+                        (Click to add new name)
+                    </Link>
+                </div>
+                <div className="col">
+                    <h4 style={{ fontWeight: "bold" }}>Points</h4>
+                </div>
+            </div>
+            {childData.map((data, index) => {
+                return (
+                    <div className="row" key={index} style={{ paddingTop: "2%", textAlign: "center" }}>
+                        <div className="shadow-bg p-2 rounded"
+                            style={{ backgroundColor: "#FFE3FC" }}>
+                            <div className="row">
+                                <div className="col"
+                                    style={{ borderRight: "2px solid black" }}>
+                                    <p style={{ paddingTop: "10px", fontSize: "18px", fontWeight: "500" }}>
+                                        {data.name}
+                                    </p>
+                                </div>
+                                <div className="col">
+                                    <div className="row">
+                                        <div className="col">
+                                            <i
+                                                onClick={() => decrementRating(data._id)}
+                                                className="btn material-icons"
+                                                style={{ cursor: "pointer", fontSize: "25px" }}>
+                                                remove_circle
+                                            </i>
+                                        </div>
+                                        <div className="col">
+                                            <p style={{ paddingTop: "10px", fontSize: "18px", fontWeight: "500" }}>{data.rating}</p>
+                                        </div>
+                                        <div className="col">
+                                            <i
+                                                onClick={() => incrementRating(data._id)}
+                                                className="btn material-icons"
+                                                style={{ cursor: "pointer", fontSize: "25px" }}>
+                                                add_circle
+                                            </i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
